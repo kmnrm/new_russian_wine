ccdb5d28861ccc54306f978f0f87d929b920060c
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-02-06 14:27:41 +0800

    Move the winery age text into the template

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ env = Environment(
 
 template = env.get_template('template.html')
 
-winery_age = 'Уже {} лет с нами'.format(utils.get_the_age(1920))
+winery_age = utils.get_the_age(1920)
 beverages_catalog = utils.create_beverages_catalog('products.txt')
 categories = [*beverages_catalog]
 
